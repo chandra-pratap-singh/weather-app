faa0a3fb1673925d74db7cdcc2405361610a90b7
Chandra Pratap <chandra_pratap@epam.com>
2019-12-17 17:43:45 +0530

    uploaded project

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+ window.onload = async ()=>{
+     document.getElementById("city-button").addEventListener('click',async ()=>{
+        document.getElementById('output').innerHTML = "Loading..."
+        const city = document.getElementById("city").value;
+        const url = `/weather?city=${city}`;
+        const response = await fetch(url);
+        const myJson = await response.json();
+        if(myJson.error)
+        {
+            document.getElementById('output').innerHTML = myJson.message;
+        }
+        else{
+            document.getElementById('output').innerHTML = `
+            <table class="table table-bordered">
+            <tr>
+            <td>Temperature:</td>
+            <td>${myJson.data.temperature}</td>
+            </tr>
+            <tr>
+            <td>Humidity:</td>
+            <td>${myJson.data.humidity}</td>
+            </tr>
+            <tr>
+            <td>Cloud Cover:</td>
+            <td>${myJson.data.cloudCover}</td>
+            </tr>
+            <tr>
+            <td>Visibility:</td>
+            <td>${myJson.data.visibility}</td>
+            </tr>
+            <tr>
+            <td>Summary:</td>
+            <td>${myJson.data.summary}</td>
+            </tr>
+            <tr>
+            <td>Location:</td>
+            <td>${myJson.data.location}</td>
+            </tr>
+            </table>
+            `;
+        }
+        console.log(JSON.stringify(myJson));
+     })
+ }
---
+++ src/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const getgrid = require('./getgrid');
+const getforcast = require('./getforcast');
+
+const app = express();
+const partialpath = path.join(__dirname,'../partials');
+const viewpath = path.join(__dirname,'../views');
+const publicPath = path.join(__dirname, '../public');
+
+
+
+app.set('view engine','hbs');
+app.set('views', viewpath);
+hbs.registerPartials(partialpath);
+
+app.use(express.static(publicPath));
+
+app.get("",(req,res)=>{
+    res.render('index');
+})
+
+app.get("/weather",(req,res)=>{
+    const query = req.query;
+    if(query.city)
+    {
+        getgrid(query.city,(error,data)=>{
+            if(error){
+                resData = {error:true,message:"Unable to locate the location"}
+                res.send(resData);
+                return console.log(data);
+                }
+            else{
+                getforcast(data.longitude, data.lattitude,data.location, (error,data)=>{
+                    if(error)
+                    {
+                        resData = {error:true,message:"Sorry unable to get the forcast"}
+                        res.send(resData);
+                        return console.log(data);
+                    }
+                    else{
+                        resData = {error:false,data:data}
+                        res.send(resData);
+                        console.log("Data sent successfully");
+                    }
+                })
+            }
+        })
+    }
+    else
+        res.send({error:true,message:"please enter a city name!"});
+    
+})
+
+app.listen(3000,()=>{
+    console.log("server listening at port 3000");
+})
